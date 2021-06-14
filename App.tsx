fb76f6d853fcff7d295fb702e513771a2e142cb7
yezii <1126yezy@gmail.com>
2021-06-14 12:24:07 +0900

    AppLoading Setting

--- App.tsx
+++ App.tsx
@@ -1,8 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = async () => {
+    const fontToLoad = [Ionicons.font];
+    const fontPromises = fontToLoad.map((font) => Font.loadAsync(font));
+    const imageToLoad = [require("./assets/coffee-icon.png")];
+    const imagePromises = imageToLoad.map((image) => Asset.loadAsync(image));
+    await Promise.all<any>([...fontPromises, ...imagePromises]);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -14,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
